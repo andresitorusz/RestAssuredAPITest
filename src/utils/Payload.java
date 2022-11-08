b76c482f16b11a6ca505349e09a83a7733a541a1
andresitorusz <andresitorusz.19@gmail.com>
2022-11-08 20:39:36 +0700

    Add dynamic json reader func

---
+++ src/utils/Payload.java
@@ -0,0 +1,40 @@
+package utils;
+
+public class Payload {
+	
+	public static String AddPlace() {
+		return "{\n"
+				+ "    \"location\": {\n"
+				+ "        \"lat\": -38.123456,\n"
+				+ "        \"lng\": 33.123456\n"
+				+ "    },\n"
+				+ "    \"accuracy\": 50,\n"
+				+ "    \"name\": \"Location Unknown\",\n"
+				+ "    \"phone_number\": \"+(62) 821 7272 8267\",\n"
+				+ "    \"address\": \"30, Side Layout, Cohen 09\",\n"
+				+ "    \"types\": [\n"
+				+ "        \"home\"\n"
+				+ "    ],\n"
+				+ "    \"website\": \"http://location-u.com\",\n"
+				+ "    \"language\": \"Eng-IN\"\n"
+				+ "}";
+	}
+	
+	public static String UpdatePlace(String placeId, String newAddress) {
+		return "{\n"
+				+ "    \"place_id\": \"" + placeId + "\",\n"
+				+ "    \"address\": \"" + newAddress + "\",\n"
+				+ "    \"key\": \"qaclick123\"\n"
+				+ "}";
+	}
+	
+	public static String AddBook(String bookName, String isbn, String aisle, String author) {
+		return "{\n"
+				+ "    \"name\": \"" + bookName + "\",\n"
+				+ "    \"isbn\": \"" + isbn + "\",\n"
+				+ "    \"aisle\": \"" + aisle + "\",\n"
+				+ "    \"author\": \"" + author + "\"\n"
+				+ "}";
+	}
+
+}
